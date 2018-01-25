f9dc76c39975d448a549b67f937244bb6431fb78
Egor Smorgunov <egorsmorgunov@gmail.com>
2018-01-25 12:45:43 +0300

    Update mysqli_native_moodle_database.php
    быстрофикс

--- lib/dml/mysqli_native_moodle_database.php
+++ lib/dml/mysqli_native_moodle_database.php
@@ -181,7 +181,7 @@ class mysqli_native_moodle_database extends moodle_database {
         }
 
         // get the default database engine
-        $sql = "SELECT @@storage_engine";
+        $sql = "SELECT @@default_storage_engine";
         $this->query_start($sql, NULL, SQL_QUERY_AUX);
         $result = $this->mysqli->query($sql);
         $this->query_end($result);
